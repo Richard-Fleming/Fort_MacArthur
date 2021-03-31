73dfcd84c44c8c193e2f0cab12b2e9e6c8cc01ae
AllexisAlvarico <c00242855@itcarlow.ie>
2021-03-31 14:16:15 +0100

    added the drawer tab

--- fort_macarthur/lib/homepage.dart
+++ fort_macarthur/lib/homepage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'size_config.dart';
+import 'size_config.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -10,20 +11,47 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: SizeConfig.backroundCOLOR,
-        body: SafeArea(
-            child: Container(
-                padding: EdgeInsets.all(20.0),
-                child: Column(children: <Widget>[
-                  Container(
-                    height: 250,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      image: DecorationImage(
-                          image: AssetImage("assets/logo/logo.png"),
-                          fit: BoxFit.contain),
-                    ),
-                  )
-                ]))));
+      appBar: AppBar(title: Text("home")),
+      backgroundColor: SizeConfig.backroundCOLOR,
+      body: SafeArea(
+          child: Container(
+              padding: EdgeInsets.all(20.0),
+              child: Column(children: <Widget>[
+                Container(
+                  height: 250,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    image: DecorationImage(
+                        image: AssetImage("assets/logo/logo.png"),
+                        fit: BoxFit.contain),
+                  ),
+                )
+              ]))),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('Home'),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+            ListTile(
+              title: Text('Item 1'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('Item 2'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
