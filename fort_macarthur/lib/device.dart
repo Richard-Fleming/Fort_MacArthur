1235dc1b53d179d4e3d8b49d38c6f69503c0e60d
John Sinnott <C00242525@itcarlow.ie>
2021-04-01 14:39:03 +0100

    Added comments for the sake of making the class more readable

--- fort_macarthur/lib/device.dart
+++ fort_macarthur/lib/device.dart
@@ -4,15 +4,23 @@ import 'dart:ui' as ui;
 import 'dart:math' as Math;
 
 class Device {
-  static double devicePixelRatio = ui.window.devicePixelRatio;
-  static ui.Size size = ui.window.physicalSize;
-  static double width = size.width;
+  static double devicePixelRatio =
+      ui.window.devicePixelRatio; // Gets our pixel count
+  static ui.Size size = ui.window.physicalSize; // The entire size of the phone
+  static double width = size.width; //
   static double height = size.height;
   static double screenWidth = width / devicePixelRatio;
   static double screenHeight = height / devicePixelRatio;
   static ui.Size screenSize = new ui.Size(screenWidth, screenHeight);
-  final bool isTablet, isPhone, isIos, isAndroid, isIphoneX, hasNotch;
-  static Device? _device;
+
+  final bool isTablet,
+      isPhone,
+      isIos,
+      isAndroid,
+      isIphoneX,
+      hasNotch; // The different device types We are interested in. This willprobably be added to eventually
+
+  static Device? _device; // ? denotes that the type can be null
   static Function? onMetricsChange;
 
   static double _safeAreaHorizontal = 0.0; // space without headers/footer
@@ -23,6 +31,7 @@ class Device {
   static const Color backroundCOLOR =
       Color(0xffc5c0ad); // currently grey but liable to change
 
+  // We need to know what device we are running on
   Device(
       {required this.isTablet,
       required this.isPhone,
@@ -34,11 +43,13 @@ class Device {
   factory Device.get() {
     if (_device != null) return _device!;
 
+    // onMetricsChange lets us know if our orientation has flipped
     if (onMetricsChange == null) {
       onMetricsChange = ui.window.onMetricsChanged;
       ui.window.onMetricsChanged = () {
-        _device = null;
+        _device = null; // our old stored metrics no longer apply
 
+        // repopulate the screen data
         size = ui.window.physicalSize;
         width = size.width;
         height = size.height;
@@ -57,6 +68,7 @@ class Device {
     bool isIphoneX = false;
     bool hasNotch = false;
 
+    // The DPR is measured in physical pixels not logical pixels
     if (devicePixelRatio < 2 && (width >= 1000 || height >= 1000)) {
       isTablet = true;
       isPhone = false;
@@ -134,12 +146,14 @@ class Device {
         (ui.window.viewPadding.top + ui.window.viewPadding.bottom));
   }
 
+  // ppi = pixels per inch
   static int get _ppi => Platform.isAndroid
       ? 160
       : Platform.isIOS
           ? 150
           : 96;
 
+  // Do we have a appbar or footer establised on this page? Used to subtract these values from screen height
   static bool _hasTopOrBottomPadding() {
     final padding = ui.window.viewPadding;
     //print(padding);
