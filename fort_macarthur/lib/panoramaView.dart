e66870b3c33a3cdd7641cd966fcab57e8f91ea38
MichalKrzyzanowski <c00240696@itcarlow.ie>
2021-04-01 15:53:24 +0100

    added test pano images for osgood and farley batteries

--- fort_macarthur/lib/panoramaView.dart
+++ fort_macarthur/lib/panoramaView.dart
@@ -2,15 +2,43 @@ import 'package:flutter/material.dart';
 import 'package:panorama/panorama.dart';
 
 class PanoView extends StatelessWidget {
+  final String title;
+  final String panoPath;
+
+  PanoView({Key? key, required this.title, required this.panoPath})
+      : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Test Pano"),
+        title: Text(title + " Battery"),
       ),
-      body: Panorama(
-        child: Image.asset("assets/images/test_pano.jpg"),
+      body: Stack(
+        alignment: Alignment.topRight,
+        children: [
+          Panorama(
+            child: Image.asset(panoPath),
+            maxLatitude: 30,
+            minLatitude: -30,
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 5, horizontal: 5),
+            child: ElevatedButton.icon(
+              onPressed: () {},
+              icon: Icon(Icons.collections_bookmark),
+              label: Text(title + " Collection"),
+            ),
+          ),
+        ],
       ),
     );
   }
+}
+
+class PanoData {
+  String title;
+  String panoPath;
+
+  PanoData({required this.title, required this.panoPath});
 }
--- fort_macarthur/lib/virtual_tours.dart
+++ fort_macarthur/lib/virtual_tours.dart
@@ -2,6 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:fort_macarthur/panoramaView.dart';
 
 class VirtualTours extends StatelessWidget {
+  final List<PanoData> batteryData = [
+    PanoData(title: "Osgood", panoPath: "assets/images/osgood_pano.jpg"),
+    PanoData(title: "Farley", panoPath: "assets/images/farley_pano.jpg")
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,14 +14,25 @@ class VirtualTours extends StatelessWidget {
         title: Text("Map Overview"),
       ),
       body: Stack(
+        alignment: Alignment.center,
         children: [
           Center(child: Image.asset("assets/images/fort_macarthur_map.png")),
-          ElevatedButton(
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => PanoView()));
-              },
-              child: Text("Pano Test"))
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: batteryData
+                .map((data) => ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => PanoView(
+                                    title: data.title,
+                                    panoPath: data.panoPath,
+                                  )));
+                    },
+                    child: Text(data.title + " Battery Test")))
+                .toList(),
+          )
         ],
       ),
     );
