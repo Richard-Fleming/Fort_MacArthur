ff29c57239a0c24999b3ad3b74ed9fedd4b4d632
MichalKrzyzanowski <c00240696@itcarlow.ie>
2021-04-08 09:46:23 +0100

    first version of the post history page done

--- fort_macarthur/lib/post_history.dart
+++ fort_macarthur/lib/post_history.dart
@@ -11,22 +11,42 @@ class PostHistoryView extends StatelessWidget {
       body: Column(
         children: [
           Image.asset("assets/images/batteryOsgood.jpg"),
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            "Battery Test",
+            style: TextStyle(
+              fontSize: 20,
+            ),
+          ),
           Expanded(
             child: Container(
               margin: EdgeInsets.all(20),
               child: GridView.count(
                 crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
                 crossAxisCount: 3,
                 children: [
                   InfoBox(
-                    color: Colors.red,
-                    iconData: Icons.ac_unit,
+                    color: Colors.deepPurple,
+                    iconData: Icons.article_outlined,
                     label: "History",
                   ),
                   InfoBox(
-                    color: Colors.blue,
-                    iconData: Icons.ac_unit,
+                    color: Colors.deepPurple,
+                    iconData: Icons.analytics_outlined,
                     label: "Details",
+                  ),
+                  InfoBox(
+                    color: Colors.deepPurple,
+                    iconData: Icons.photo,
+                    label: "Slideshow",
+                  ),
+                  InfoBox(
+                    color: Colors.deepPurple,
+                    iconData: Icons.military_tech,
+                    label: "Artifacts",
                   ),
                 ],
               ),
