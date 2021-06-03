869d86e86cfc90aa4e6d9b43841485df4299615e
MichalKrzyzanowski <c00240696@itcarlow.ie>
2021-06-03 12:43:40 +0100

    fixed nav bar issue

--- fort_macarthur/lib/aboutus.dart
+++ fort_macarthur/lib/aboutus.dart
@@ -12,79 +12,84 @@ class AboutUsPage extends StatefulWidget {
 class _AboutUsPageState extends State<AboutUsPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      color: Device.backroundCOLOR,
-      child: ListView(
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.all(
-              SizeConstraint.edgeInsets / 2.0,
-            ),
-            child: Container(
-              height: 360,
-              width: 100,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage("assets/logo/logo.png"),
-                    fit: BoxFit.contain),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Learn More"),
+      ),
+      body: Container(
+        alignment: Alignment.center,
+        color: Device.backroundCOLOR,
+        child: ListView(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(
+                SizeConstraint.edgeInsets / 2.0,
               ),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(
-              SizeConstraint.edgeInsets,
-            ),
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(
-                  SizeConstraint.borderRadius,
+              child: Container(
+                height: 360,
+                width: 100,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("assets/logo/logo.png"),
+                      fit: BoxFit.contain),
                 ),
-                color: Colors.white,
-              ),
-              child: Column(
-                children: [
-                  createText("\nAbout the Museum", EdgeInsets.zero, 24.0,
-                      FontWeight.bold),
-                  Divider(
-                    height: 40,
-                    thickness: 2.0,
-                    color: Colors.black,
-                  ),
-                  createText(
-                      "The Fort MacArthur Museum is dedicated to the " +
-                          "preservation and interpretation of the history " +
-                          "of Fort MacArthur, a U.S. Army post which guarded" +
-                          " the Los Angeles harbor from 1914 to 1974. The " +
-                          "Museum was established in 1985, and it is housed" +
-                          " in the corridors and galleries of historic" +
-                          " Battery Osgood-Farley. The Fort MacArthur" +
-                          " Reservations hold an important collection of" +
-                          " historical structures which were part of the U.S." +
-                          " Army's role in the defense of the American" +
-                          " continental coastline from invasion. These" +
-                          " structures clearly trace the development of" +
-                          " American coastal defenses, from the all big gun" +
-                          " era of the turn of the century to the missile " +
-                          "era of today.\n\n" +
-                          "The rooms, galleries and corridors of the" +
-                          " Museum contain a variety of exhibits and" +
-                          " displays which include: the history of " +
-                          "Los Angeles harbor defenses, home-front " +
-                          "activities in the greater Los Angeles area " +
-                          "during the World Wars, Civil Defense, " +
-                          "American Pacific Theater military campaigns, " +
-                          "early American Air Defenses and the important " +
-                          "role of Los Angeles as a military port for " +
-                          "both the Army and the Navy.\n",
-                      EdgeInsets.zero,
-                      16.0,
-                      FontWeight.bold),
-                ],
               ),
             ),
-          )
-        ],
+            Padding(
+              padding: EdgeInsets.all(
+                SizeConstraint.edgeInsets,
+              ),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(
+                    SizeConstraint.borderRadius,
+                  ),
+                  color: Colors.white,
+                ),
+                child: Column(
+                  children: [
+                    createText("\nAbout the Museum", EdgeInsets.zero, 24.0,
+                        FontWeight.bold),
+                    Divider(
+                      height: 40,
+                      thickness: 2.0,
+                      color: Colors.black,
+                    ),
+                    createText(
+                        "The Fort MacArthur Museum is dedicated to the " +
+                            "preservation and interpretation of the history " +
+                            "of Fort MacArthur, a U.S. Army post which guarded" +
+                            " the Los Angeles harbor from 1914 to 1974. The " +
+                            "Museum was established in 1985, and it is housed" +
+                            " in the corridors and galleries of historic" +
+                            " Battery Osgood-Farley. The Fort MacArthur" +
+                            " Reservations hold an important collection of" +
+                            " historical structures which were part of the U.S." +
+                            " Army's role in the defense of the American" +
+                            " continental coastline from invasion. These" +
+                            " structures clearly trace the development of" +
+                            " American coastal defenses, from the all big gun" +
+                            " era of the turn of the century to the missile " +
+                            "era of today.\n\n" +
+                            "The rooms, galleries and corridors of the" +
+                            " Museum contain a variety of exhibits and" +
+                            " displays which include: the history of " +
+                            "Los Angeles harbor defenses, home-front " +
+                            "activities in the greater Los Angeles area " +
+                            "during the World Wars, Civil Defense, " +
+                            "American Pacific Theater military campaigns, " +
+                            "early American Air Defenses and the important " +
+                            "role of Los Angeles as a military port for " +
+                            "both the Army and the Navy.\n",
+                        EdgeInsets.zero,
+                        16.0,
+                        FontWeight.bold),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
@@ -127,4 +132,3 @@ class _AboutUsPageState extends State<AboutUsPage> {
 }
 
 // Creates a text element with padding
-
